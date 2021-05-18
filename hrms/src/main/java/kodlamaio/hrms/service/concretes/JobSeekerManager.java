4896928975a5d5677e3e33711a63dac447d5b5c1
Beyza Kaynak <beyzakaynak98@gmail.com>
2021-05-18 23:35:54 +0300

    regex doğrulaması eklendi

--- hrms/src/main/java/kodlamaio/hrms/service/concretes/JobSeekerManager.java
+++ hrms/src/main/java/kodlamaio/hrms/service/concretes/JobSeekerManager.java
@@ -8,6 +8,8 @@ import kodlamaio.hrms.dataAccess.abstracts.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 public class JobSeekerManager {
 
@@ -25,20 +27,26 @@ public class JobSeekerManager {
 
         Boolean isTrue = mernisAdapterService.verifyNationalityId(jobSeekerDto);
         if(isTrue){
-            User user = new User();
-            user.setEmail(jobSeekerDto.getEmail());
-            user.setPassword(jobSeekerDto.getPassword());
+           if(Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(jobSeekerDto.getEmail()).matches()){
+               User user = new User();
+               user.setEmail(jobSeekerDto.getEmail());
+               user.setPassword(jobSeekerDto.getPassword());
 
-            User inDb = userDao.save(user);
 
-            JobSeeker jobSeeker = new JobSeeker();
-            jobSeeker.setFirsName(jobSeekerDto.getFirstName());
-            jobSeeker.setLastName(jobSeekerDto.getLastName());
-            jobSeeker.setNationalId(jobSeekerDto.getNationalId());
-            jobSeeker.setYearOfBirth(jobSeekerDto.getYearOfBirth());
-            jobSeeker.setUser(inDb);
+               User inDb = userDao.save(user);
 
-            jobSeekerDao.save(jobSeeker);
+               JobSeeker jobSeeker = new JobSeeker();
+               jobSeeker.setFirsName(jobSeekerDto.getFirstName());
+               jobSeeker.setLastName(jobSeekerDto.getLastName());
+               jobSeeker.setNationalId(jobSeekerDto.getNationalId());
+               jobSeeker.setYearOfBirth(jobSeekerDto.getYearOfBirth());
+               jobSeeker.setUser(inDb);
+
+               jobSeekerDao.save(jobSeeker);
+           }else{
+               throw new Exception("Emailiniz hatalıdır.");
+           }
+
         } else {
             throw new Exception("Bilgileriniz hatalıdır.");
         }
